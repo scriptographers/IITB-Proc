e4aacc53889a68ee1b34f80b2db5920808685b15
Harshit Varma <varmaharshit2@gmail.com>
2021-05-21 18:36:37 +0530

    Updated onebitadder

--- code/OneBitAdder.vhd
+++ code/OneBitAdder.vhd
@@ -1,18 +1,20 @@
+-- Component: Single Bit Full Adder
+library work;
+use work.all;
 library ieee;
 use ieee.std_logic_1164.all;
-library work;
-use work.Gates.all;
 
 entity OneBitAdder is
   port (
-    A, B, Cin : in std_logic;
-    S, Cout   : out std_logic);
+    a, b, cin : in std_logic;
+    sum, cout : out std_logic
+  );
 end OneBitAdder;
 
 architecture Struct of OneBitAdder is
 begin
   
-  S <= A xor B xor Cin;
-  Cout <= (A and B) or (A and Cin) or (B and Cin);
+  sum <= a xor b xor cin;
+  cout <= (a and b) or (a and cin) or (b and cin);
 
-end Struct;
+end architecture;
--- code/SixteenBitAdder.vhd
+++ code/SixteenBitAdder.vhd
@@ -1,31 +1,37 @@
+-- Component: 16 Bit Full Adder
+library work;
+use work.all;
 library ieee;
 use ieee.std_logic_1164.all;
-library work;
-use work.Gates.all;
 
 entity SixteenBitAdder is
   port (
     in_a : in std_logic_vector (15 downto 0);
     in_b : in std_logic_vector (15 downto 0);
-    sum  : out std_logic_vector (16 downto 0));
+    sum  : out std_logic_vector (16 downto 0)
+  );
 end SixteenBitAdder;
 
 architecture Struct of SixteenBitAdder is
 
   component OneBitAdder is
     port (
-      A, B, Cin : in std_logic;
-      S, Cout   : out std_logic);
+      a, b, cin : in std_logic;
+      sum, cout : out std_logic
+    );
   end component OneBitAdder;
+
   signal tC : std_logic_vector(16 downto 0);
-  
+
 begin
 
   tC(0) <= '0';
 
   add : for i in 15 downto 0 generate
-    ax : OneBitAdder port map(A => in_a(i), B => in_b(i), Cin => tC(i), S => sum(i), Cout => tC(i + 1));
+    ax : OneBitAdder 
+    port map(a => in_a(i), b => in_b(i), cin => tC(i), sum => sum(i), cout => tC(i + 1));
   end generate add;
+
   sum(16) <= tC(16);
 
 end Struct;
