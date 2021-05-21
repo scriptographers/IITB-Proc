4fad0eaded44dbbacce659d25237b2e5cfb127d2
devansh-dvj <devansh.dvj@gmail.com>
2021-05-21 19:59:49 +0530

    Added set of instructions in memory file

--- code/Memory_asyncread_syncwrite.vhd
+++ code/Memory_asyncread_syncwrite.vhd
@@ -17,7 +17,7 @@ entity Memory_asyncread_syncwrite is
 		Mem_dataout         : out std_logic_vector(15 downto 0));
 end entity;
 
-architecture Form of Memory_asyncread_syncwrite is
+architecture struct of Memory_asyncread_syncwrite is
 
 	type regarray is array(31 downto 0) of std_logic_vector(15 downto 0); -- defining a new type
 	signal Memory : regarray := (
@@ -43,15 +43,38 @@ architecture Form of Memory_asyncread_syncwrite is
 		24 => x"ffff",
 		26 => x"ffff",
 		27 => x"0012",
-		others => x"0000");
+		others => x"0000"
+	);
+	-- Uncomment the following to get another set of instructions
+	-- signal Memory : regarray := (
+	-- 	0 => x"3000",
+	-- 	1 => x"3000",
+	-- 	2 => x"1057",
+	-- 	3 => x"4442",
+	-- 	4 => x"0458",
+	-- 	5 => x"2460",
+	-- 	6 => x"2921",
+	-- 	7 => x"1111",
+	-- 	8 => x"2921",
+	-- 	9 => x"58c0",
+	-- 	10 => x"7292",
+	-- 	11 => x"6e60",
+	-- 	12 => x"c040",
+	-- 	13 => x"127f",
+	-- 	14 => x"c241",
+	-- 	16 => x"9440",
+	-- 	22 => x"83f5",
+	-- 	25 => x"ffed",
+	-- 	26 => x"ffff",
+	-- 	others => x"0000"
+	-- );
 
-	-- 1 => x"3000",2 => x"1057",3 => x"4442",4 => x"0458",5 => x"2460",6 => x"2921",7 => x"1111",8 => x"2921",9 => x"58c0",10 => x"7292",11 => x"6e60",12 => x"c040",13 => x"127f",14 => x"c241",16 => x"9440",22 => x"83f5",25 => x"ffed",others => "0000000000000000");
+	-- you can use the below mentioned way to initialise the memory with the instructions and the data as required to test your processor
 
-	-- you can use the above mentioned way to initialise the memory with the instructions and the data as required to test your processor
 begin
+
 	Mem_dataout <= Memory(conv_integer(address));
-	Mem_write :
-	process (Mem_wrbar, Mem_datain, address, clk)
+	Mem_write : process (Mem_wrbar, Mem_datain, address, clk)
 	begin
 		if (Mem_wrbar = '0') then
 			if (rising_edge(clk)) then
@@ -59,4 +82,5 @@ begin
 			end if;
 		end if;
 	end process;
-end Form;
+
+end struct;
