d18406ffa82b8cd5282cb8fcc8f07297ff2d89a7
devansh-dvj <devansh.dvj@gmail.com>
2021-05-21 19:48:31 +0530

    Updated variable names

--- code/iitb_proc.vhd
+++ code/iitb_proc.vhd
@@ -94,14 +94,14 @@ architecture Form of iitb_proc is
 			carry, zero, done, alucont, m12 : out std_logic);
 	end component;
 
-	signal m1out, m2out, pcout, alu_a, alu_b, alu_c, t1out, t2out, t3out, memout, irout,
-	Imm9e16, m5out, D1out, D2out, m8out, m7out, m12out, m6out, seImm9, seImm6 : std_logic_vector(15 downto 0);
+	signal PC_out, IR_out, ALU_a, ALU_b, ALU_c, T1_out, T2_out, T3_out, Mem_out, D1_out, D2_out,
+	M1_out, M2_out, M5_out, M6_out, M7_out, M8_out, M12_out, Imm9e16, SEImm9, SEImm6 : std_logic_vector(15 downto 0);
 
-	signal tm4out, tm3out : std_logic_vector(2 downto 0);
+	signal M4_out, M3_out : std_logic_vector(2 downto 0);
 
 	signal W1, W2, W3, W4, W5, W7, W6,
 	M1, M21, M20, M4, M30, M31, M50, M51, M8, M70, M71, M61, M60, M91, M90, M100, M101, M12,
-	Z_out, C_out, WC, Cr_out, T1_zero, Mux, M11_out, WZ, Zr_out, alu_control : std_logic;
+	Z_out, C_out, WC, Cr_out, T1_zero, mux, M11_out, WZ, Zr_out, alu_control : std_logic;
 
 	signal temp1 : std_logic_vector(9 downto 0);
 
@@ -113,209 +113,209 @@ architecture Form of iitb_proc is
 
 begin
 
-	fsm1 : FSM
+	FSM_en : FSM
 	port map(
 		-- in
-		instruction => irout, T1 => m8out, T2 => m7out, T3 => t3out,
+		instruction => IR_out, T1 => M8_out, T2 => M7_out, T3 => T3_out,
 		r => reset, clk => clk, init_carry => Cr_out, init_zero => Zr_out,
 		-- out
 		w1 => W1, w2 => W2, w3 => W3, w4 => W4, w5 => W5, w6 => W6, w7 => W7,
 		m1 => M1, m20 => M20, m21 => M21, m30 => M30, m31 => M31, m4 => M4,
 		m50 => M50, m51 => M51, m8 => M8, m70 => M70, m71 => M71, m61 => M61, m60 => M60,
-		m91 => M91, m90 => M90, m100 => M100, m101 => M101, mux => Mux, carry => WC,
-		zero => WZ, done => Done, alucont => alu_control, mem => memout, m12 => M12
+		m91 => M91, m90 => M90, m100 => M100, m101 => M101, mux => mux, carry => WC,
+		zero => WZ, done => Done, alucont => alu_control, mem => Mem_out, m12 => M12
 	);
 
-	Mux1 : MUX16_2x1
+	MUX1 : MUX16_2x1
 	port map(
 		-- in
-		A => alu_c, B => t2out,
+		A => ALU_c, B => T2_out,
 		-- select
 		S0 => M1,
 		-- out
-		y => m1out
+		y => M1_out
 	);
 
 	PC : Register16
 	port map(
 		-- in
-		Reg_datain => m1out, clk => clk,
+		Reg_datain => M1_out, clk => clk,
 		-- control pin
 		Reg_wrbar => W1,
 		-- out
-		Reg_dataout => pcout
+		Reg_dataout => PC_out
 	);
 
-	Mux2 : MUX16_4x1
+	MUX2 : MUX16_4x1
 	port map(
 		-- in
-		A => t2out, B => pcout, C => alu_c, D => t1out,
+		A => T2_out, B => PC_out, C => ALU_c, D => T1_out,
 		-- select
 		S1 => M21, S0 => M20,
 		-- out
-		y => m2out
+		y => M2_out
 	);
 
 	Mem1 : Memory_asyncread_syncwrite
 	port map(
 		-- in
-		address => m2out, Mem_datain => m12out, clk => clk,
+		address => M2_out, Mem_datain => M12_out, clk => clk,
 		-- control pin
 		Mem_wrbar => W2,
 		-- out
-		Mem_dataout => memout
+		Mem_dataout => Mem_out
 	);
 
 	IR : Register16
 	port map(
 		-- in
-		Reg_datain => memout, clk => clk,
+		Reg_datain => Mem_out, clk => clk,
 		-- control pin
 		Reg_wrbar => W3,
 		--out
-		Reg_dataout => irout
+		Reg_dataout => IR_out
 	);
 
-	Mux4 : MUX3_2x1
+	MUX4 : MUX3_2x1
 	port map(
 		-- in
-		A => irout(11 downto 9), B => t3out(2 downto 0),
+		A => IR_out(11 downto 9), B => T3_out(2 downto 0),
 		-- select
 		S0 => M4,
 		-- out
-		y => tm4out
+		y => M4_out
 	);
 
-	Mux3 : MUX3_4x1
+	MUX3 : MUX3_4x1
 	port map(
 		-- in
-		A => irout(11 downto 9), B => irout(8 downto 6),
-		C => irout(5 downto 3), D => t3out(2 downto 0),
+		A => IR_out(11 downto 9), B => IR_out(8 downto 6),
+		C => IR_out(5 downto 3), D => T3_out(2 downto 0),
 		-- select
 		S1 => M31, S0 => M30,
 		-- out
-		y => tm3out
+		y => M3_out
 	);
 
-	Imm9e16 <= irout(8 downto 0) & "0000000";
+	Imm9e16 <= IR_out(8 downto 0) & "0000000";
 
-	temp1 <= (others => irout(5));
+	temp1 <= (others => IR_out(5));
 
-	seImm6 <= temp1 & irout(5 downto 0);
+	SEImm6 <= temp1 & IR_out(5 downto 0);
 
-	temp2 <= (others => irout(8));
+	temp2 <= (others => IR_out(8));
 
-	seImm9 <= temp2 & irout(8 downto 0);
+	SEImm9 <= temp2 & IR_out(8 downto 0);
 
-	Mux5 : MUX16_4x1
+	MUX5 : MUX16_4x1
 	port map(
 		-- in
-		A => pcout, B => Imm9e16, C => t2out, D => t3out,
+		A => PC_out, B => Imm9e16, C => T2_out, D => T3_out,
 		-- select
 		S1 => M51, S0 => M50,
 		-- out
-		y => m5out
+		y => M5_out
 	);
 
 	Rf : Register_file
 	port map(
 		-- in
-		address1 => tm4out, address2 => irout(8 downto 6), address3 => tm3out,
-		Reg_datain3 => m5out, clk => clk,
+		address1 => M4_out, address2 => IR_out(8 downto 6), address3 => M3_out,
+		Reg_datain3 => M5_out, clk => clk,
 		-- control pin
 		Reg_wrbar => W4,
 		-- out
-		Reg_dataout1 => D1out, Reg_dataout2 => D2out
+		Reg_dataout1 => D1_out, Reg_dataout2 => D2_out
 	);
 
-	Mux8 : MUX16_2x1
+	MUX8 : MUX16_2x1
 	port map(
 		-- in
-		A => D1out, B => alu_c,
+		A => D1_out, B => ALU_c,
 		-- select
 		S0 => M8,
 		-- out
-		y => m8out
+		y => M8_out
 	);
 
-	Mux7 : MUX16_4x1
+	MUX7 : MUX16_4x1
 	port map(
 		-- in
-		A => D1out, B => D2out, C => alu_c, D => memout,
+		A => D1_out, B => D2_out, C => ALU_c, D => Mem_out,
 		-- select
 		S0 => M70, S1 => M71,
 		--out
-		y => m7out
+		y => M7_out
 	);
 
-	Mux6 : MUX16_4x1
+	MUX6 : MUX16_4x1
 	port map(
 		-- in
-		A => memout, B => Z16, C => alu_c, D => Z16,
+		A => Mem_out, B => Z16, C => ALU_c, D => Z16,
 		-- select
 		S1 => M61, S0 => M60,
 		--out
-		y => m6out
+		y => M6_out
 	);
 
 	T1_reg : Register16
 	port map(
 		-- in
-		Reg_datain => m8out, clk => clk,
+		Reg_datain => M8_out, clk => clk,
 		-- control pin
 		Reg_wrbar => W7,
 		--out
-		Reg_dataout => t1out
+		Reg_dataout => T1_out
 	);
 
 	T2_reg : Register16
 	port map(
 		-- in
-		Reg_datain => m7out, clk => clk,
+		Reg_datain => M7_out, clk => clk,
 		-- control pin
 		Reg_wrbar => W6,
 		--out
-		Reg_dataout => t2out
+		Reg_dataout => T2_out
 	);
 
 	T3_reg : Register16
 	port map(
 		-- in
-		Reg_datain => m6out, clk => clk,
+		Reg_datain => M6_out, clk => clk,
 		-- control pin
 		Reg_wrbar => W5,
 		-- out
-		Reg_dataout => t3out
+		Reg_dataout => T3_out
 	);
 
-	Mux9 : MUX16_4x1
+	MUX9 : MUX16_4x1
 	port map(
 		-- in
-		A => seImm9, B => seImm6, C => t2out, D => O16,
+		A => SEImm9, B => SEImm6, C => T2_out, D => O16,
 		-- select
 		S1 => M91, S0 => M90,
 		-- out
-		y => alu_b
+		y => ALU_b
 	);
 
-	Mux10 : MUX16_4x1
+	MUX10 : MUX16_4x1
 	port map(
 		-- in
-		A => t3out, B => pcout, C => t1out, D => t2out,
+		A => T3_out, B => PC_out, C => T1_out, D => T2_out,
 		-- select
 		S1 => M101, S0 => M100,
 		-- out
-		y => alu_a
+		y => ALU_a
 	);
 
 	ALU_en : ALU
 	port map(
 		-- in
-		a => alu_a, b => alu_b,
+		a => ALU_a, b => ALU_b,
 		-- control pin
 		op => alu_control,
 		-- out
-		c => alu_c,
+		c => ALU_c,
 		--out flags
 		z => Z_out, cout => C_out
 	);
@@ -330,15 +330,15 @@ begin
 		Reg_dataout => Cr_out
 	);
 
-	T1_zero <= not(t1out(0) or t1out(1) or t1out(2) or t1out(3) or t1out(4) or t1out(5) or t1out(6) or t1out(7)
-		or t1out(8) or t1out(9) or t1out(10) or t1out(11) or t1out(12) or t1out(13) or t1out(14) or t1out(15));
+	T1_zero <= not(T1_out(0) or T1_out(1) or T1_out(2) or T1_out(3) or T1_out(4) or T1_out(5) or T1_out(6) or T1_out(7)
+		or T1_out(8) or T1_out(9) or T1_out(10) or T1_out(11) or T1_out(12) or T1_out(13) or T1_out(14) or T1_out(15));
 
-	Mux11 : MUX1_2x1
+	MUX11 : MUX1_2x1
 	port map(
 		-- in
 		A => Z_out, B => T1_zero,
 		-- select
-		S0 => Mux,
+		S0 => mux,
 		-- out
 		y => M11_out
 	);
@@ -353,16 +353,16 @@ begin
 		Reg_dataout => Zr_out
 	);
 
-	Mux12 : MUX16_2x1
+	MUX12 : MUX16_2x1
 	port map(
 		-- in
-		A => t1out, B => t2out,
+		A => T1_out, B => T2_out,
 		-- select
 		S0 => M12,
 		--out
-		y => m12out
+		y => M12_out
 	);
 
-	O <= memout;
+	O <= Mem_out;
 
 end Form;
