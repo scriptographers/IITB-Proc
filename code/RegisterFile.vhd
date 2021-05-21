e8c36c4015c987bc6987a52821870e1f8707f03c
Harshit Varma <varmaharshit2@gmail.com>
2021-05-21 23:33:48 +0530

    Updated regfile

--- code/RegisterFile.vhd
+++ code/RegisterFile.vhd
@@ -4,9 +4,6 @@ use work.all;
 library ieee;
 use ieee.std_logic_1164.all;
 
-library std;
-use std.standard.all;
-
 use ieee.numeric_std.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
@@ -16,16 +13,17 @@ entity RegisterFile is
 		address1, address2, address3 : in std_logic_vector(2 downto 0);
 		Reg_datain3                  : in std_logic_vector(15 downto 0);
 		clk, write_flag              : in std_logic;
-		Reg_dataout1, Reg_dataout2   : out std_logic_vector(15 downto 0));
+		Reg_dataout1, Reg_dataout2   : out std_logic_vector(15 downto 0)
+	);
 end entity;
 
-architecture struct of RegisterFile is
+architecture arch of RegisterFile is
 
 	type regarray is array(7 downto 0) of std_logic_vector(15 downto 0);
-	signal RegisterF : regarray := (1 => x"0000", 2 => x"0000", 3 => x"0000",
-	4 => x"0000", 5 => x"0000", 6 => x"0000", 7 => x"0000", others => x"0000");
+	signal RegisterF : regarray := (others => x"0000");
 
 begin
+
 	Reg_dataout1 <= RegisterF(conv_integer(address1));
 	Reg_dataout2 <= RegisterF(conv_integer(address2));
 
@@ -38,4 +36,4 @@ begin
 		end if;
 	end process;
 
-end struct;
+end architecture;
