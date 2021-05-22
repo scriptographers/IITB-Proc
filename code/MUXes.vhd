960302e77b581746977a5a1b526029b118b2c0a7
Harshit Varma <varmaharshit2@gmail.com>
2021-05-22 09:48:18 +0530

    Updated MUXes

--- code/MUXes.vhd
+++ code/MUXes.vhd
@@ -1,54 +1,67 @@
--- Component: Package of MUXes
+-- Component: Package of MUXes used
 library work;
 use work.all;
 library ieee;
 use ieee.std_logic_1164.all;
 
 package MUXes is
+	
+	-- (a) MUX1_2x1: 2 inputs, 1 output, 1 selector, each input is a single bit
 	component MUX1_2x1 is
 		port (
 			A, B, S0 : in std_logic;
-			y        : out std_logic);
+			y        : out std_logic
+		);
 	end component MUX1_2x1;
 
+	-- (b) MUX1_4x1: 4 inputs, 1 output, 2 selectors, each input is a single bit
 	component MUX1_4x1 is
 		port (
 			A, B, C, D, S1, S0 : in std_logic;
-			y                  : out std_logic);
+			y                  : out std_logic
+		);
 	end component MUX1_4x1;
 
+	-- (c) MUX3_2x1: 2 inputs, 1 output, 1 selector, each input is a 3-bit vector
 	component MUX3_2x1 is
 		port (
 			A, B : in std_logic_vector(2 downto 0);
 			S0   : in std_logic;
-			y    : out std_logic_vector(2 downto 0));
+			y    : out std_logic_vector(2 downto 0)
+		);
 	end component MUX3_2x1;
 
+	-- (d) MUX3_4x1: 4 inputs, 1 output, 2 selectors, each input is a 3-bit vector
 	component MUX3_4x1 is
 		port (
 			A, B, C, D : in std_logic_vector(2 downto 0);
 			S1, S0     : in std_logic;
-			y          : out std_logic_vector(2 downto 0));
+			y          : out std_logic_vector(2 downto 0)
+		);
 	end component MUX3_4x1;
 
+	-- (e) MUX16_2x1: 2 inputs, 1 output, 1 selector, each input is a 16-bit vector
 	component MUX16_2x1 is
 		port (
 			A, B : in std_logic_vector(15 downto 0);
 			S0   : in std_logic;
-			y    : out std_logic_vector(15 downto 0));
+			y    : out std_logic_vector(15 downto 0)
+		);
 	end component MUX16_2x1;
 
+	-- (f) MUX16_4x1: 4 inputs, 1 output, 2 selectors, each input is a 16-bit vector
 	component MUX16_4x1 is
 		port (
 			A, B, C, D : in std_logic_vector(15 downto 0);
 			S1, S0     : in std_logic;
-			y          : out std_logic_vector(15 downto 0));
+			y          : out std_logic_vector(15 downto 0)
+		);
 	end component MUX16_4x1;
 
 end package MUXes;
 
 
--- MUX1_2x1
+-- (a) MUX1_2x1: 2 inputs, 1 output, 1 selector, each input is a single bit
 library work;
 use work.all;
 library ieee;
@@ -57,16 +70,17 @@ use ieee.std_logic_1164.all;
 entity MUX1_2x1 is
 	port (
 		A, B, S0 : in std_logic;
-		y        : out std_logic);
+		y        : out std_logic
+	);
 end MUX1_2x1;
 
 architecture arch of MUX1_2x1 is
 begin
 	y <= (A and not S0) or (B and S0);
-end arch;
+end architecture;
 
 
--- MUX1_4x1
+-- (b) MUX1_4x1: 4 inputs, 1 output, 2 selectors, each input is a single bit
 library work;
 use work.all;
 library ieee;
@@ -75,7 +89,8 @@ use ieee.std_logic_1164.all;
 entity MUX1_4x1 is
 	port (
 		A, B, C, D, S1, S0 : in std_logic;
-		y                  : out std_logic);
+		y                  : out std_logic
+	);
 end MUX1_4x1;
 
 architecture arch of MUX1_4x1 is
@@ -84,9 +99,10 @@ begin
 		or (B and (not S1) and (S0))
 		or (C and (S1) and (not S0))
 		or (D and (S1) and (S0));
-end arch;
+end architecture;
 
--- MUX3_2x1
+
+-- (c) MUX3_2x1: 2 inputs, 1 output, 1 selector, each input is a 3-bit vector
 library work;
 use work.all;
 library ieee;
@@ -96,22 +112,25 @@ entity MUX3_2x1 is
 	port (
 		A, B : in std_logic_vector(2 downto 0);
 		S0   : in std_logic;
-		y    : out std_logic_vector(2 downto 0));
+		y    : out std_logic_vector(2 downto 0)
+	);
 end MUX3_2x1;
 
 architecture arch of MUX3_2x1 is
 	component MUX1_2x1 is
 		port (
 			A, B, S0 : in std_logic;
-			y        : out std_logic);
+			y        : out std_logic
+		);
 	end component;
 begin
 	MUXg : for i in 2 downto 0 generate
 		mx : MUX1_2x1 port map(A => A(i), B => B(i), S0 => S0, y => y(i));
 	end generate MUXg;
-end arch;
+end architecture;
 
--- MUX3_4x1
+
+-- (d) MUX3_4x1: 4 inputs, 1 output, 2 selectors, each input is a 3-bit vector
 library work;
 use work.all;
 library ieee;
@@ -121,22 +140,25 @@ entity MUX3_4x1 is
 	port (
 		A, B, C, D : in std_logic_vector(2 downto 0);
 		S1, S0     : in std_logic;
-		y          : out std_logic_vector(2 downto 0));
+		y          : out std_logic_vector(2 downto 0)
+	);
 end MUX3_4x1;
 
 architecture arch of MUX3_4x1 is
 	component MUX1_4x1 is
 		port (
 			A, B, C, D, S1, S0 : in std_logic;
-			y                  : out std_logic);
+			y                  : out std_logic
+		);
 	end component;
 begin
 	MUXg : for i in 2 downto 0 generate
 		mx : MUX1_4x1 port map(A => A(i), B => B(i), C => C(i), D => D(i), S0 => S0, S1 => S1, y => y(i));
 	end generate MUXg;
-end arch;
+end architecture;
 
--- MUX16_2x1
+
+-- (e) MUX16_2x1: 2 inputs, 1 output, 1 selector, each input is a 16-bit vector
 library work;
 use work.all;
 library ieee;
@@ -146,22 +168,25 @@ entity MUX16_2x1 is
 	port (
 		A, B : in std_logic_vector(15 downto 0);
 		S0   : in std_logic;
-		y    : out std_logic_vector(15 downto 0));
+		y    : out std_logic_vector(15 downto 0)
+	);
 end MUX16_2x1;
 
 architecture arch of MUX16_2x1 is
 	component MUX1_2x1 is
 		port (
 			A, B, S0 : in std_logic;
-			y        : out std_logic);
+			y        : out std_logic
+		);
 	end component;
 begin
 	MUXg : for i in 15 downto 0 generate
 		mx : MUX1_2x1 port map(A => A(i), B => B(i), S0 => S0, y => y(i));
 	end generate MUXg;
-end arch;
+end architecture;
 
--- MUX16_4x1
+
+-- (f) MUX16_4x1: 4 inputs, 1 output, 2 selectors, each input is a 16-bit vector
 library work;
 use work.all;
 library ieee;
@@ -171,17 +196,19 @@ entity MUX16_4x1 is
 	port (
 		A, B, C, D : in std_logic_vector(15 downto 0);
 		S1, S0     : in std_logic;
-		y          : out std_logic_vector(15 downto 0));
+		y          : out std_logic_vector(15 downto 0)
+	);
 end MUX16_4x1;
 
 architecture arch of MUX16_4x1 is
 	component MUX1_4x1 is
 		port (
 			A, B, C, D, S1, S0 : in std_logic;
-			y                  : out std_logic);
+			y                  : out std_logic
+		);
 	end component;
 begin
 	MUXg4 : for i in 15 downto 0 generate
 		mx : MUX1_4x1 port map(A => A(i), B => B(i), C => C(i), D => D(i), S0 => S0, S1 => S1, y => y(i));
 	end generate MUXg4;
-end arch;
+end architecture;
