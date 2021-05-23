d3aa47058bba66971b566810db55953d8a22f5b7
devansh-dvj <devansh.dvj@gmail.com>
2021-05-23 05:36:42 +0530

    Added LHI Test - works

--- code/Memory.vhd
+++ code/Memory.vhd
@@ -20,29 +20,39 @@ architecture arch of Memory is
 
 	-- A new type: Array of 32 elements, where each element is a 16-bit vector
 	type MemoryArray is array(31 downto 0) of std_logic_vector(15 downto 0);
+	-- Original Test
+	-- signal mem : MemoryArray := (
+	-- 	0 => x"4054",
+	-- 	1 => x"6000",
+	-- 	2 => x"c042",
+	-- 	3 => x"0210",
+	-- 	4 => x"c4c3",
+	-- 	7 => x"13be",
+	-- 	8 => x"2128",
+	-- 	9 => x"0a32",
+	-- 	10 => x"c982",
+	-- 	11 => x"212a",
+	-- 	12 => x"3caa",
+	-- 	13 => x"5044",
+	-- 	14 => x"8202",
+	-- 	16 => x"91c0",
+	-- 	18 => x"7000",
+	-- 	19 => x"f000",
+	-- 	20 => x"0014",
+	-- 	21 => x"0002",
+	-- 	23 => x"0016",
+	-- 	24 => x"ffff",
+	-- 	26 => x"ffff",
+	-- 	27 => x"0012",
+	-- 	others => x"0000"
+	-- );
+
+	-- LHI Test
 	signal mem : MemoryArray := (
-		0 => x"4054",
-		1 => x"6000",
-		2 => x"c042",
-		3 => x"0210",
-		4 => x"c4c3",
-		7 => x"13be",
-		8 => x"2128",
-		9 => x"0a32",
-		10 => x"c982",
-		11 => x"212a",
-		12 => x"3caa",
-		13 => x"5044",
-		14 => x"8202",
-		16 => x"91c0",
-		18 => x"7000",
-		19 => x"f000",
-		20 => x"0014",
-		21 => x"0002",
-		23 => x"0016",
-		24 => x"ffff",
-		26 => x"ffff",
-		27 => x"0012",
+		0 => x"3009", -- LHI R0 9
+		1 => x"33ff", -- LHI R1 511
+		2 => x"3400", -- LHI R2 0
+		3 => x"3601", -- LHI R3 1
 		others => x"0000"
 	);
 
@@ -51,7 +61,7 @@ begin
 	-- Read
 	data_read <= mem(conv_integer(addr));
 
-	proc_write : process(write_flag, data_write, addr, clk)
+	proc_write : process (write_flag, data_write, addr, clk)
 	begin
 		if (write_flag = '0') then
 			if (rising_edge(clk)) then
