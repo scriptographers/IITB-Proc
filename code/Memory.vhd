c0bfd6c11d8856148f7ceb2e2c391e68a4e32fc5
devansh-dvj <devansh.dvj@gmail.com>
2021-05-23 16:08:23 +0530

    Added LW-SW Test - works

--- code/Memory.vhd
+++ code/Memory.vhd
@@ -18,8 +18,8 @@ end entity;
 
 architecture arch of Memory is
 
-	-- A new type: Array of 32 elements, where each element is a 16-bit vector
-	type MemoryArray is array(31 downto 0) of std_logic_vector(15 downto 0);
+	-- A new type: Array of 64 elements, where each element is a 16-bit vector
+	type MemoryArray is array(63 downto 0) of std_logic_vector(15 downto 0);
 	-- Original Test
 	-- signal mem : MemoryArray := (
 	-- 	0 => x"4054",
@@ -66,23 +66,35 @@ architecture arch of Memory is
 	-- );
 
 	-- ADD-NAND Test
+	-- signal mem : MemoryArray := (
+	-- 	0 => x"31ff", -- LHI R0 511
+	-- 	1 => x"33fe", -- LHI R1 510
+	-- 	2 => x"3401", -- LHI R2 1
+	-- 	3 => x"3602", -- LHI R3 2
+	-- 	4 => x"04e0", -- ADD R2 R3 R4
+	-- 	5 => x"24e0", -- NDU R2 R3 R4
+	-- 	6 => x"00e2", -- ADC R0 R3 R4
+	-- 	7 => x"00e1", -- ADZ R0 R3 R4
+	-- 	8 => x"20e2", -- NDC R0 R3 R4
+	-- 	9 => x"20e1", -- NDZ R0 R3 R4
+	-- 	10 => x"00e0", -- ADD R0 R3 R4
+	-- 	11 => x"00a2", -- ADC R0 R2 R4
+	-- 	12 => x"00e1", -- ADZ R0 R3 R4
+	-- 	13 => x"2ba2", -- NDC R5 R6 R4
+	-- 	14 => x"2922", -- NDC R4 R4 R4
+	-- 	15 => x"20e1", -- NDZ R0 R3 R4
+	-- 	others => x"0000"
+	-- );
+
+	-- LW-SW Test
 	signal mem : MemoryArray := (
-		0 => x"31ff", -- LHI R0 511
-		1 => x"33fe", -- LHI R1 510
-		2 => x"3401", -- LHI R2 1
-		3 => x"3602", -- LHI R3 2
-		4 => x"04e0", -- ADD R2 R3 R4
-		5 => x"24e0", -- NDU R2 R3 R4
-		6 => x"00e2", -- ADC R0 R3 R4
-		7 => x"00e1", -- ADZ R0 R3 R4
-		8 => x"20e2", -- NDC R0 R3 R4
-		9 => x"20e1", -- NDZ R0 R3 R4
-		10 => x"00e0", -- ADD R0 R3 R4
-		11 => x"00a2", -- ADC R0 R2 R4
-		12 => x"00e1", -- ADZ R0 R3 R4
-		13 => x"2ba2", -- NDC R5 R6 R4
-		14 => x"2922", -- NDC R4 R4 R4
-		15 => x"20e1", -- NDZ R0 R3 R4
+		0 => x"101f", -- ADI R0 R0 31
+		1 => x"1043", -- ADI R0 R1 3
+		2 => x"4402", -- LW R2 R0 2
+		3 => x"3696", -- LHI R3 150
+		4 => x"5604", -- SW R3 R0 4
+		5 => x"4841", -- LW R4 R1 1
+		33 => x"ff00", -- Memory address 33
 		others => x"0000"
 	);
 
