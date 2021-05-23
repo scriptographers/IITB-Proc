da6c4256362a54cc2a754e077e55b5f9a179e0cf
devansh-dvj <devansh.dvj@gmail.com>
2021-05-23 19:36:28 +0530

    Sorted ordering of Muxes and write flags in FSM

--- code/FSM.vhd
+++ code/FSM.vhd
@@ -25,39 +25,42 @@ begin
 	process (rst, clk, instruction, init_carry, init_zero, T1, T2, T3, fsm_state_symbol)
 		variable state_v : StateSymbol;
 		variable W_PC_v, W_MEM_v, W_IR_v, W_RF_v, W_T3_v, W_T2_v, W_T1_v,
-		M1_v, M20_v, M21_v, M40_v, M41_v, M3_v, M50_v, M51_v, M80_v, M81_v,
-		M70_v, M71_v, M6_v, M90_v, M91_v, M100_v, M101_v, M12_v,
+		M1_v, M20_v, M21_v, M3_v, M40_v, M41_v, M50_v, M51_v, M6_v,
+		M70_v, M71_v, M80_v, M81_v, M90_v, M91_v, M100_v, M101_v, M12_v,
 		carry_v, zero_v, M11_v, done_v, alu_v : std_logic;
 
 	begin
 
 		state_v := fsm_state_symbol;
+
 		W_PC_v := '0';
 		W_MEM_v := '0';
 		W_IR_v := '0';
 		W_RF_v := '0';
-		W_T3_v := '0';
-		W_T2_v := '0';
 		W_T1_v := '0';
+		W_T2_v := '0';
+		W_T3_v := '0';
+
 		M1_v := '0';
 		M20_v := '1';
 		M21_v := '0';
+		M3_v := '0';
 		M40_v := '0';
 		M41_v := '0';
-		M3_v := '0';
 		M50_v := '0';
 		M51_v := '0';
-		M80_v := '0';
-		M81_v := '0';
+		M6_v := '0';
 		M70_v := '0';
 		M71_v := '0';
-		M6_v := '0';
+		M80_v := '0';
+		M81_v := '0';
 		M90_v := '0';
 		M91_v := '0';
 		M100_v := '0';
 		M101_v := '0';
 		M11_v := '0';
 		M12_v := '0';
+
 		carry_v := '1';
 		zero_v := '1';
 		done_v := '0';
@@ -81,14 +84,14 @@ begin
 				end if;
 
 			when S0 =>
-				W_T3_v := '1';
-				W_T2_v := '1';
 				W_T1_v := '1';
-				M80_v := '1';
-				M81_v := '0';
+				W_T2_v := '1';
+				W_T3_v := '1';
+				M6_v := '0';
 				M70_v := '1';
 				M71_v := '0';
-				M6_v := '0';
+				M80_v := '1';
+				M81_v := '0';
 
 				if (instruction(15 downto 13) = "010") then
 					state_v := S4;
@@ -214,13 +217,13 @@ begin
 				state_v := Sf;
 
 			when S7A =>
-				W_T2_v := '1';
 				W_T1_v := '1';
+				W_T2_v := '1';
 				M20_v := '1';
 				M21_v := '1';
+				M6_v := '1';
 				M70_v := '1';
 				M71_v := '1';
-				M6_v := '1';
 				M90_v := '1';
 				M91_v := '1';
 				M100_v := '0';
@@ -249,13 +252,13 @@ begin
 				end if;
 
 			when S8A =>
-				W_T3_v := '1';
 				W_T2_v := '1';
+				W_T3_v := '1';
 				M3_v := '1';
-				M80_v := '0';
-				M81_v := '1';
 				M70_v := '0';
 				M71_v := '0';
+				M80_v := '0';
+				M81_v := '1';
 				M90_v := '1';
 				M91_v := '1';
 				M100_v := '0';
@@ -345,28 +348,30 @@ begin
 		W_MEM <= W_MEM_v;
 		W_IR <= W_IR_v;
 		W_RF <= W_RF_v;
-		W_T3 <= W_T3_v;
-		W_T2 <= W_T2_v;
 		W_T1 <= W_T1_v;
+		W_T2 <= W_T2_v;
+		W_T3 <= W_T3_v;
+
 		M1 <= M1_v;
 		M20 <= M20_v;
 		M21 <= M21_v;
+		M3 <= M3_v;
 		M40 <= M40_v;
 		M41 <= M41_v;
-		M3 <= M3_v;
 		M50 <= M50_v;
 		M51 <= M51_v;
-		M80 <= M80_v;
-		M81 <= M81_v;
+		M6 <= M6_v;
 		M70 <= M70_v;
 		M71 <= M71_v;
-		M6 <= M6_v;
+		M80 <= M80_v;
+		M81 <= M81_v;
 		M90 <= M90_v;
 		M91 <= M91_v;
 		M100 <= M100_v;
 		M101 <= M101_v;
 		M11 <= M11_v;
 		M12 <= M12_v;
+
 		carry_write <= carry_v;
 		zero_write <= zero_v;
 		done <= done_v;
